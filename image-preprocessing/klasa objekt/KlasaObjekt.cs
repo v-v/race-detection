5ba2ec5b050dcf291b9eb62f3b8562e9090ed3dd
Mr-Lemur <ds.greenmonk@gmail.com>
2012-12-21 19:22:42 +0100

    Klasa za objekte slika, napravljena tako da sadrži ime slike i klasu, kasnije ću dodati dio da možemo napraviti random odabir objekata.

---
+++ image-preprocessing/klasa objekt/KlasaObjekt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ucitavanjeSlika
+{
+    class KlasaObjekt
+    {
+        private string klasaOznaka;
+        private string imeSlike;
+
+        public KlasaObjekt()
+        {
+            this.klasaOznaka = "-";
+            this.imeSlike = "-";
+        }
+        public KlasaObjekt(string ime, string klasa) 
+        {
+            this.klasaOznaka = klasa;
+            this.imeSlike = ime;
+        }
+        public void setIme(string ime)
+        {
+            this.imeSlike = ime;
+        }
+        public string getIme()
+        {
+            return this.imeSlike;
+        }
+        public void setKlasa(string klasa) {
+            this.klasaOznaka = klasa;
+        }
+        public string getKlasa()
+        {
+            return this.klasaOznaka;
+        }
+    }
+    class TestListaObjekata
+    {
+        static void Main()
+        {
+            List<KlasaObjekt> lista = new List<KlasaObjekt>();
+            for (int i = 0; i < 10; i++) 
+            { 
+                KlasaObjekt obj = new KlasaObjekt("testObjekt",i.ToString());
+                lista.Add(obj);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(lista[i].getIme());
+                Console.Write(" : ");
+                Console.WriteLine(lista[i].getKlasa());
+            }
+        }
+    }
+}
