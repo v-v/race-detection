fbb5f260e60416f61d4c2e198f130bb016babc98
Slaven Mišak <slaven.misak@gmail.com>
2012-12-22 19:42:46 +0100

    Dodan LBP i pocetak WLD-a
    LBP je prekopiran s interneta ali trebal bi raditi. Uzima i vraća Bitmap
    objekt. Implementacija WLD-a je u tijeku.

---
+++ image-processing/RASPUZO/ImageProcessing/WLD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ImageProcessing
+{
+    public class WLD
+    {
+        public static Bitmap WLDAlgorithm(Bitmap graySource, int width, int height)
+        {
+            var bmp = graySource;
+            var numRow = height;
+            var numCol = width;
+            var gray = new Bitmap(width, height);
+
+            int blockSizeY = 3;
+            int blockSizeX = 3;
+            int belta = 5;
+            int alpha = 3;
+            double epsilon = 1e-7;
+            int brojSusjeda = 8;
+
+            if(numCol < blockSizeX || numRow < blockSizeY)
+                throw new Exception("Slika je premala!");
+
+            var filter = new[,]
+                                {
+                                    {1, 1, 1},
+                                    {1, -8, 1},
+                                    {1, 1, 1}
+                                };
+
+            int dx = width - blockSizeX;
+            int dy = height - blockSizeY;
+
+            return null;
+        }
+    }
+}
---
+++ image-processing/README.md
@@ -0,0 +1,3 @@
+LBP radi, WLD bude
+
+Dodano testiranje da vidimo kaj se dogodi sa slikama
