c0d206cc5c7cb5d0368423ae83d63e2f2429860a
Mr-Lemur <ds.greenmonk@gmail.com>
2012-12-31 01:42:24 +0100

    Nešto sam pokušavao kombinirajući sa onim Andrijinim učitavanjem, ali nisam siguran jel to ok.

---
+++ image-preprocessing/ucitavanjeSlika/KlasaObjekt.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+
+namespace ucitavanjeSlika
+{
+    class KlasaObjekt
+    {
+        private string klasaOznaka;
+        private string imeSlike;
+        private Image<Bgr, Byte> img;
+
+        public KlasaObjekt()
+        {
+            this.klasaOznaka = "-";
+            this.imeSlike = "-";
+        }
+        public KlasaObjekt(Image<Bgr,Byte> image,string ime, string klasa) 
+        {
+            this.klasaOznaka = klasa;
+            this.imeSlike = ime;
+            this.img = image;
+        }
+        public void setIme(string ime)
+        {
+            this.imeSlike = ime;
+        }
+        public string getIme()
+        {
+            return this.imeSlike;
+        }
+        public void setKlasa(string klasa) {
+            this.klasaOznaka = klasa;
+        }
+        public string getKlasa()
+        {
+            return this.klasaOznaka;
+        }
+        public void setImage(Image<Bgr, Byte> image){
+            this.img = image;
+        }
+        public Image<Bgr, Byte> getImage()
+        {
+            return this.img;
+        }
+    }
+    class TestListaObjekata
+    {
+        static void Main()
+        {
+            List<KlasaObjekt> lista = new List<KlasaObjekt>();
+            List<string> listaOznaka = new List<string>();
+            string imagesResourcePath1 = ".\\data\\images\\broj\\*fa*";
+            string imagesResourcePath2 = ".\\data\\images\\broj\\*fb*";
+            string imageDataFile = ".\\data\\ground_truths\\name_value\\broj.txt";
+            string[] imagesFileNames1 = Directory.GetFiles(imagesResourcePath1);
+            string[] imagesFileNames2 = Directory.GetFiles(imagesResourcePath2);
+            string[] lines = System.IO.File.ReadAllLines(imageDataFile);
+
+            foreach (string line in lines)
+            {
+                listaOznaka.Add(line);
+            }
+            int counter = 0;
+            int totalCount1 = imagesFileNames1.Length;
+            foreach (string imageName in imagesFileNames1)
+            {
+                try
+                {
+                    counter++;
+                    Console.WriteLine("Processing image " + counter + " of " + totalCount1);
+                    Image < Bgr, Byte> img = new Image < Bgr, Byte>(imageName);
+                    KlasaObjekt obj = new KlasaObjekt(img,imageName,listaOznaka[counter]);
+                    lista.Add(obj);
+
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(imageName + ": " + e.Message);
+                    Console.ReadLine();
+                }
+            }
+            counter = 0;
+            int totalCount2 = imagesFileNames2.Length;
+            foreach (string imageName in imagesFileNames2)
+            {
+                try
+                {
+                    counter++;
+                    Console.WriteLine("Processing image " + counter + " of " + totalCount2);
+                    Image<Bgr, Byte> img = new Image<Bgr, Byte>(imageName);
+                    KlasaObjekt obj = new KlasaObjekt(img, imageName, listaOznaka[counter]);
+                    lista.Add(obj);
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(imageName + ": " + e.Message);
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Finished processing. Enter to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
