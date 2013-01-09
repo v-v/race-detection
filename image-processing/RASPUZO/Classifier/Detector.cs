263bd3261644f55f410b0f5f0fe85b52188c4a0b
v-v <v-v@proxima.(none)>
2013-01-09 20:46:08 +0100

    Izrezivanje lica i resizanje grayscale slike na WxH (preporuceno 100x100 kao predobrada za LBP

--- image-processing/RASPUZO/Classifier/Detector.cs
+++ image-processing/RASPUZO/Classifier/Detector.cs
@@ -32,20 +32,30 @@ namespace Classifier
             imgKozaLica = imgKozaLica.Copy(mask);
 
             imgKozaLica.AvgSdv(out colorAvg, out colorSdv);
-
-            // ---- end boja koze ----
-
-            // predobrada slike za LBP (pretvaranje u CB + resizanje na fiksnu velicinu
-            //Image<Bgr, Byte> imgCBFixedSize = img.Convert(Image<Gray, byte>);
           
         }
 
         public static Image<Gray, Byte> VratiCrnoBijelu(Image<Bgr, Byte> slika, int width, int height)
         {
-            var novi = slika.Convert<Gray, byte>();
-            novi.Resize(width, height, INTER.CV_INTER_LINEAR);
+            var imgLiceZaLBP = new Image<Gray, Byte>(100, 100);
+            var imgLiceTmp = new Image<Bgr, Byte>(img.Width, img.Height);
 
-            return novi;
+            var haar = new HaarCascade("haarcascade_frontalface_alt.xml");
+            var faces = img.DetectHaarCascade(haar)[0];
+
+            foreach (var face in faces)
+            {
+                if (face.rect != null)
+                {
+                    imgLiceTmp = img.GetSubRect(face.rect);
+                    break;
+                }
+            }
+
+            imgLiceZaLBP = imgLiceTmp.Convert<Gray, byte>();
+            imgLiceZaLBP.Reize(width, height, INTER.CV_INTER_LINEAR);
+
+            return imgLiceZaLBP;
         } 
     }
 }
